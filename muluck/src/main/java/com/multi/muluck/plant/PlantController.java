8fb0cb0d025a1c57bb39f3ae414683114729db4f
DASOM <n980805@gmail.com>
2023-05-16 17:51:50 +0900

    Add files via upload

--- muluck/src/main/java/com/multi/muluck/plant/PlantController.java
+++ muluck/src/main/java/com/multi/muluck/plant/PlantController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("plant")
+
 @Controller
 public class PlantController {
 
@@ -15,40 +16,11 @@ public class PlantController {
 	PlantDAO dao;
 
 	/*
-	 * @RequestMapping("insert") public vodi asdfasdf()_ {
-	 * 
-	 * }
-	 */
-
-	/*
 	 * @RequestMapping("/plant") public void landing(Model model) {
-	 * 
 	 * System.out.println("야호 "); // Model은 컨트롤러의 list를 views/list.jsp까지만 전달할 수 있는
 	 * 객체 List<PlantVO> list = dao.list(); model.addAttribute("list", list);
 	 * System.out.println("list : "+ list); }
 	 */
-
-	@RequestMapping("insert")
-	public void insert(PlantVO bag) {
-		System.out.println("insert요청됨.");
-		System.out.println(bag);
-		System.out.println(dao);
-		dao.insert(bag);
-	}
-
-	@RequestMapping("update")
-	public void update(PlantVO bag) {
-		System.out.println("update요청됨.");
-		System.out.println(bag);
-		dao.update(bag);
-	}
-
-	@RequestMapping("delete")
-	public void delete(int plant_no) {
-		System.out.println("delete요청됨.");
-		System.out.println(plant_no);
-		dao.delete(plant_no);
-	}
 
 	@RequestMapping("one")
 	public void one(String plant_species, Model model) {
@@ -58,9 +30,11 @@ public class PlantController {
 		model.addAttribute("bag", bag);
 	}
 
-	@RequestMapping("list")
-	public void list(Model model) { // Model은 컨트롤러의 list를views/list.jsp까지만 전달할 수 있는 객체
-		List<PlantVO> list = dao.list();
-		model.addAttribute("list", list);
+	@RequestMapping("plant")
+	public void list(Model model) { // Model은 컨트롤러의 list를 views/list.jsp까지만 전달할 수 있는 객체
+	List<PlantVO> list = dao.list();
+	model.addAttribute("list", list);
 	}
+	
+	
 }
